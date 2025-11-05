5003ed25f7604621a55848507beeb4cab33bbfb6
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-05 11:58:44 -0300

    Add project files.

---
+++ ContainerizeNET/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ContainerizeNET
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}

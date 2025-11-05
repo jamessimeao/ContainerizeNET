a79541799f14cf59d9c2372055905fd9e54ddb65
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-05 12:01:11 -0300

    print a number each second

--- ContainerizeNET/Program.cs
+++ ContainerizeNET/Program.cs
@@ -1,14 +1,17 @@
-﻿namespace ContainerizeNET
+﻿using System.Threading.Tasks;
+
+namespace ContainerizeNET
 {
     internal class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             int i = 0;
             while (true)
             {
                 Console.WriteLine(i);
                 i++;
+                await Task.Delay(TimeSpan.FromSeconds(1));
             }
         }
     }
